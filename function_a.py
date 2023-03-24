06c38732eacec5647f450518f86e9ed6c0417d7b
Jasmin Worthy <jasminworthy@gmail.com>
2023-03-24 16:24:21 -0500

    added func_a

--- function_a.py
+++ function_a.py
@@ -1,9 +1,17 @@
 def most_common_value(number_list):
     """ returns the most common element of the list
     """
-    pass
+    frequency_index = {}
+    max_frequency = -1
+    most_common_value = None
+    for num in number_list:
+        if frequency_index.get(num):
+            frequency_index[num] += 1
+        else:
+            frequency_index[num] = 1
 
+        if max_frequency < frequency_index[num]:
+            max_frequency = frequency_index[num]
+            most_common_value = num
 
-if __name__ == "__main__":
-    nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
-    print(f"Most common value = {most_common_value(nums)}")
+    return most_common_value
